bf5fdc2cfb965373eaf5b6bfe3c968d45dda2390
Francisco Cunha Neto <fcunhaneto@gmail.com>
2018-08-01 10:35:32 -0300

    Create views in home with basic TemplateView.

--- home/views.py
+++ home/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 
+from django.views.generic.base import TemplateView
+from django.views.generic.base import View
+from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html')
@@ -9,3 +12,18 @@ def home(request):
 def my_logout(request):
     logout(request)
     return redirect('home')
+
+
+class HomePageView(TemplateView):
+
+    template_name = "home2.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['home3_var'] = 'minha var em home3'
+        return context
+
+
+class MyView(View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse('Hello, World!')
